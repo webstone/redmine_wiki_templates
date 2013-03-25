6c0948fabc7457c678f598a88e22284675b4d78b
ucho <ucho@nts.ricoh.co.jp>
2013-03-25 16:44:03 +0900

    confirming & flash notice.

--- lib/wiki_templates_application_hooks.rb
+++ lib/wiki_templates_application_hooks.rb
@@ -1,4 +1,5 @@
 class WikiTemplatesApplicationHooks < Redmine::Hook::ViewListener
+  render_on :view_layouts_base_html_head, :partial => 'wiki_templates/html_head'
   render_on :view_layouts_base_body_bottom, :partial => 'wiki_templates/body_bottom'
 end
 
