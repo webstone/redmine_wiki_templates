de9608884f25c315da0ae7cf1361f9491fc9d574
Daigo UCHIYAMA <ucho@nts.ricoh.co.jp>
2016-11-18 10:24:12 +0900

    Update version.

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_wiki_templates do
   name 'Redmine Wiki Templates plugin'
   author 'Marta Gonzalez de Chaves Aguilera, Daigo UCHIYAMA'
   description 'This plugin allow you choose a wiki template when you add a new wiki page.'
-  version '0.2.0'
+  version '0.3.0'
   url 'https://github.com/ucho/redmine_wiki_templates'
   requires_redmine :version_or_higher => '3.0.0'
   project_module :wiki_templates do
